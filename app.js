dcbc9d4838b90d0230f1fe915f96d2ca4a85f6cc
ZainHanook <134081856+ZackHalsey@users.noreply.github.com>
2023-06-27 20:22:57 +0500

    set env to prod and set heroku postbuild

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const express = require("express");
-const userRouter = require("./route/userRouter");
-const teamRouter = require("./route/teamRouter");
-const gameRouter = require("./route/gameRouter");
-const passport = require("./config/passport");
-const session = require("express-session");
-const dotenv = require("dotenv");
-const AppError=require("./utils/appError")
-const errorController=require("./controller/errorController")
-const rateLimit = require('express-rate-limit')
-const cors=require("cors")
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const userRouter = require('./route/userRouter');
+const teamRouter = require('./route/teamRouter');
+const gameRouter = require('./route/gameRouter');
+const passport = require('./config/passport');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const AppError = require('./utils/appError');
+const errorController = require('./controller/errorController');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-dotenv.config({ path: "./config.env" });
+const path = reqire('path');
+dotenv.config({ path: './config.env' });
 const app = express();
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors("*"));
+app.use(cors('*'));
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(
   session({
     secret: process.env.SESSION_SECRET_KEY,
@@ -26,22 +27,33 @@ app.use(
     saveUninitialized: true,
   })
 );
-const apilimiter=rateLimit({
-  windowMs: 5 * 60 * 1000, 
-	max: 10,
-  message:"Too many requests from this IP, please try again after 5 minutes"
-})
+const apilimiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 10,
+  message: 'Too many requests from this IP, please try again after 5 minutes',
+});
 app.use(passport.initialize());
 app.use(passport.session());
-app.use("/api/shootfolio", userRouter);
-app.use("/api/coinMarketCap",userRouter)
-app.use("/api/shootfolio/team", teamRouter);
-app.use("/api/shootfolio/game", gameRouter);
+app.use('/api/shootfolio', userRouter);
+app.use('/api/coinMarketCap', userRouter);
+app.use('/api/shootfolio/team', teamRouter);
+app.use('/api/shootfolio/game', gameRouter);
 
-app.use("/api", apilimiter);
-app.all("*", (req, res, next) => {
-  const err=new AppError(`Can't find ${req.originalUrl} on this server!`,404);
+app.use('/api', apilimiter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+app.all('*', (req, res, next) => {
+  const err = new AppError(
+    `Can't find ${req.originalUrl} on this server!`,
+    404
+  );
   next(err);
 });
 app.use(errorController);
-module.exports = app; 
+module.exports = app;
