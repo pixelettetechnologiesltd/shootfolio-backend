0230e7e31fe778784d6fc775c860f7ca8b9a7280
pixelettetechnology <99417812+pixelettetechnology@users.noreply.github.com>
2023-07-04 00:33:54 +0500

    update

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const userRouter = require("./route/userRouter");
 const teamRouter = require("./route/teamRouter");
@@ -6,25 +5,24 @@ const gameRouter = require("./route/gameRouter");
 const passport = require("./config/passport");
 const session = require("express-session");
 const dotenv = require("dotenv");
-const AppError=require("./utils/appError")
-const errorController=require("./controller/errorController")
-const rateLimit = require('express-rate-limit')
-const cors=require("cors")
-const multer  = require('multer')
-const cookieParser = require('cookie-parser')
+const AppError = require("./utils/appError");
+const errorController = require("./controller/errorController");
+const rateLimit = require("express-rate-limit");
+const cors = require("cors");
+const multer = require("multer");
+const cookieParser = require("cookie-parser");
 
-const bodyParser = require('body-parser');
-const path = require('path');
+const bodyParser = require("body-parser");
+const path = require("path");
 dotenv.config();
 const app = express();
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: "*" }));
 
-
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(
   session({
     secret: process.env.SESSION_SECRET_KEY,
@@ -35,25 +33,25 @@ app.use(
 const apilimiter = rateLimit({
   windowMs: 5 * 60 * 1000,
   max: 10,
-  message: 'Too many requests from this IP, please try again after 5 minutes',
+  message: "Too many requests from this IP, please try again after 5 minutes",
 });
 app.use(passport.initialize());
 app.use(passport.session());
-app.use('/api/shootfolio', userRouter);
-app.use('/api/coinMarketCap', userRouter);
-app.use('/api/shootfolio/team', teamRouter);
-app.use('/api/shootfolio/game', gameRouter);
+app.use("/api/shootfolio", userRouter);
+app.use("/api/coinMarketCap", userRouter);
+app.use("/api/shootfolio/team", teamRouter);
+app.use("/api/shootfolio/game", gameRouter);
 
-app.use('/api', apilimiter);
+app.use("/api", apilimiter);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   const err = new AppError(
     `Can't find ${req.originalUrl} on this server!`,
     404
