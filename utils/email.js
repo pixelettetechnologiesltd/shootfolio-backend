120ffd37441b817fa5f761175a737b973b8af5b1
pixelettetechnology <99417812+pixelettetechnology@users.noreply.github.com>
2023-07-04 12:58:52 +0500

    Update Email

--- utils/email.js
+++ utils/email.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (options,token,emailToBeSend) => {
+const sendEmail = async (options, token, emailToBeSend) => {
   const { email } = options;
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
@@ -10,32 +10,27 @@ const sendEmail = async (options,token,emailToBeSend) => {
       pass: process.env.EMAIL_PASSWORD,
     },
   });
- if(emailToBeSend=='emailVerify'){
- 
-  const mailOptions = {
-    from: process.env.EMAIL_USERNAME,
-    to: email,
-    subject: "hello from shootfolio",
-    text: "message from shootfolio",
-    html: `Click <a href="http://localhost:4000/api/shootfolio/confirm/${token}">here</a> to verify your email.`
+  if (emailToBeSend == "emailVerify") {
+    const mailOptions = {
+      from: process.env.EMAIL_USERNAME,
+      to: email,
+      subject: "hello from shootfolio",
+      text: "message from shootfolio",
+      html: `Click <a href="https://api-shootfolio.onrender.com/api/shootfolio/confirm/${token}">here</a> to verify your email.`,
+    };
 
-  };
- 
-  await transporter.sendMail(mailOptions);
-}
-else {
-  
-  const mailOptions = {
-    from: process.env.EMAIL_USERNAME,
-    to: email,
-    subject: "hello from shootfolio",
-    text: "Please Reset Your Password",
-    html: `Click <a href="http://localhost:3000/resetPassword/${token}">here</a> to reset your password.`
+    await transporter.sendMail(mailOptions);
+  } else {
+    const mailOptions = {
+      from: process.env.EMAIL_USERNAME,
+      to: email,
+      subject: "hello from shootfolio",
+      text: "Please Reset Your Password",
+      html: `Click <a href="https://shootfolio.ptlearningzone.com/resetPassword/${token}">here</a> to reset your password.`,
+    };
 
-  };
- 
-  await transporter.sendMail(mailOptions);
-}
+    await transporter.sendMail(mailOptions);
+  }
 };
 
 module.exports = sendEmail;
